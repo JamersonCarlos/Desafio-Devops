590208f85ef0199955f73c0ee7207b85f7351cdf
Jamerson Carlos <jcventurosa@gmail.com>
2022-08-12 05:16:52 -0300

    Correções de conexão com o banco de Dados
    Algumas atualizações, conseguir fazer a migração de dados. Só que os dados não foram para o local certo

--- web/docker_django/apps/todo/models.py
+++ web/docker_django/apps/todo/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Item(models.Model):
     text = models.TextField(blank=False, null=False)
     date_posted = models.DateField(auto_now=True)
-    id = models.AutoField(primary_key=True)
+    # id = models.AutoField(primary_key=True)
